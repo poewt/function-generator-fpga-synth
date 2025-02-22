3e152d6759a7cd8e24e0c7bba0443c4e98b88c92
poewt <79345528+poewt@users.noreply.github.com>
2025-02-22 00:14:54 -0500

    Add 8 point sine wave generator

---
+++ sine_8_gen_tb.v
@@ -0,0 +1,29 @@
+`timescale 1ns/1ps
+
+module sine_8_gen_tb();
+
+    // Test inputs
+    reg clk;
+
+    // Test outputs
+    wire [7:0] sine_height;
+
+    sine_8_gen uut(
+        clk,
+        sine_height
+    );
+
+    always #10 clk = ~clk;
+
+    initial begin
+        $dumpfile("sine_8_gen_tb.vcd");
+        $dumpvars(0, sine_8_gen_tb);
+
+        clk = 0;
+
+        #500_000;
+
+        $finish;
+    end
+
+endmodule
